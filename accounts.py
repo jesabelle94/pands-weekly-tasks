69875d7f06632fa14dff28416b8d05a3c569c136
Jenny Formentera <G00439444@atu.ie>
2024-02-12 21:02:06 +0000

    weekly task 03

---
+++ accounts.py
@@ -0,0 +1,13 @@
+# Weekly Task 03
+# Please enter a 10 digit account number: 1234567890 and replace the first 6 digits with Xs and show only the last 4 digits)
+# Author: Jenny Formentera
+
+'''
+accountno = input("Please enter a 10 digit account number:")
+print("XXXXXX"+accountno[6:10])
+'''
+
+# modify the program to deal with account numbers of any length
+
+accountno = input("Please enter an account number:")
+print(accountno[0:4]) #prints the last 4 digits of the account number
